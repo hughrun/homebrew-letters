c7f13c68d304e319c270f51329cd7e48d362d1c5
Hugh Rundle <17669239+hughrun@users.noreply.github.com>
2021-05-17 03:56:08 +0000

    lette.rs: add 1.2.3 bottle.

--- Formula/lette.rs.rb
+++ Formula/lette.rs.rb
@@ -5,6 +5,12 @@ class LetteRs < Formula
   sha256 "80989d7806fe446eb222b4fbfa8a0019b79196a558cce99cae99c39eb300fa25"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/hughrun/homebrew-letters/releases/download/lette.rs-1.2.3"
+    sha256 cellar: :any_skip_relocation, catalina:     "054cb68655fc78ef73df725f02ba9a2f3e3c9e26fd92007cb166f9d675ad5470"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cd4a6330309309d105caee63875e67441f1104fcb7c62f52e1009e4c8abf5f78"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl"
 
